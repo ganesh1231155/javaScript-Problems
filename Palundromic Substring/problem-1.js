412ed51f853adb536b407f4e46b0992651595921
ganesh1231155 <kolteshreeganesh1155@gmail.com>
2025-05-28 19:22:43 +0530

    Longest Palindromic substring

---
+++ Palundromic Substring/problem-1.js
@@ -0,0 +1,45 @@
+// Problem: find Longest Palindromic Substring using Dynamic Programming, we can use a 2D table dp[i][j] where:
+
+// i is the starting index
+
+// j is the ending index
+
+// dp[i][j] is true if the substring from index i to j is a palindrome
+
+function longestPalindromic(s){
+
+    if(s.length===0) return "";
+    const n=s.length;
+    let start=0;
+    let maxLen=0;
+    const dp=Array.from(Array(n),()=>Array(n).fill(false));
+    for(let i=0;i<n;i++)
+    {
+        dp[i][i]=true;
+    }
+
+    for(let i=0;i<n-1;i++){
+        if(s[i]===s[i+1]){
+            dp[i][i+1]=true;
+            start=1;
+            maxLen=2;
+        }
+    }
+
+    for(let len=3;len<n;len++){
+        for(let i=0;i<=n-len;i++){
+            let j=i+len-1;
+            if(s[i]===s[j]&&dp[i+1][j-1]){
+                dp[i][j]=true;
+                if(len>maxLen){
+                    start=i;
+                    maxLen=len;
+                }
+            }
+        }
+    }
+
+    return s.substring(start,start+maxLen);
+
+}
+console.log(longestPalindromic("babad"));
