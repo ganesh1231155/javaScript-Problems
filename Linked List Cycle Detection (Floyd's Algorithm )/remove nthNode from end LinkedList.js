ec2b1e8c56dc57f0c4a0b248577771a1f0d4c4d1
ganesh1231155 <kolteshreeganesh1155@gmail.com>
2025-06-07 13:27:41 +0530

    remove from end(LinkedList).

---
+++ Linked List Cycle Detection (Floyd's Algorithm )/remove nthNode from end LinkedList.js
@@ -0,0 +1,40 @@
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+// Example 1:
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+
+// Example 2:
+// Input: head = [1], n = 1
+// Output: []
+
+// Example 3:
+// Input: head = [1,2], n = 1
+// Output: [1]
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    if(head.next==null) return null;
+    let first=head;
+    let second=head;
+    for(let i=0;i<n;i++){
+        first=first.next;
+    }
+    if(!first) return head.next;
+    while(first.next){
+        first=first.next;
+        second=second.next;
+    }
+    second.next=second.next.next
+    return head;
+};
